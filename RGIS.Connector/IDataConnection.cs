58dd03cac25fec0f2f57d5b339c789271c09145d
Sam Rundel <samuel.rundel@run-s.de>
2021-08-18 23:56:04 +0200

    Initial Creation

---
+++ RGIS.Connector/IDataConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGIS.Connector
+{
+    public interface IDataConnection
+    {
+        public bool CheckValidity(string[][] columnNames, string[][] columnTypes);
+
+        public bool CheckConnection();
+        public bool OpenConnection();
+        public bool CloseConnection();
+        public bool Insert();
+        public bool Update();
+        public bool Delete();
+        public List<string[]> Select();
+        public int Count();
+        public bool BackUp();
+        public bool Restore();
+        public string JSON(string tableName);
+        public string JSON(string[] tableNames);
+    }
+}
---
+++ RGIS.Connector/mySQLConnector.cs
@@ -0,0 +1,117 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGIS.Connector
+{
+    public class mySQLConnector : IDataConnection
+    {
+        private MySqlConnection connection;
+
+        public mySQLConnector(string connectionString, string[][] columnNames, string[][] columnTypes)
+        {
+            connection = new MySqlConnection(connectionString);
+            OpenConnection();
+            CheckValidity(columnNames, columnTypes);
+        }
+
+        public bool CheckConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                if (!CheckConnection())
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server. Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool CheckValidity(string[][] columnNames, string[][] columnTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string[]> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BackUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Restore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string JSON(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string JSON(string[] tableNames)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
